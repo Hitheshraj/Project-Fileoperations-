c82d85fb81c677169dffeb1de42c600546e5a238
Hithesh G. Raj <hiraj@BLR.ALLEGISINDIA.COM>
2023-03-06 13:52:20 +0530

    first commit

---
+++ Fileoperations/src/Project/Main.java
@@ -0,0 +1,7 @@
+package Project;
+public class Main {
+public static void main(String[] args) {
+	System.out.println("--------WELCOME--------");
+	System.out.println("Name : Hithesh G Raj\n");
+}
+}
