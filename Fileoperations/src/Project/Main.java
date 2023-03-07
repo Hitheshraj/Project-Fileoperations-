d461e135fc98f37922e7830b8ed808ab170c7369
Hithesh G. Raj <hiraj@BLR.ALLEGISINDIA.COM>
2023-03-07 16:51:45 +0530

    updated commit

--- Fileoperations/src/Project/Main.java
+++ Fileoperations/src/Project/Main.java
@@ -10,8 +10,8 @@ public class Main {
 		System.out.println("--------WELCOME--------");
 		System.out.println("Name : Hithesh G Raj");
 		try (Scanner inp = new Scanner(System.in)) {
-			System.out.println("Select 1 -> Display Files\n" + "Select 2 -> Add Files\n" + "Select 3 -> Delete Files\n" + "Select 4 -> Search File\n"
-					+ "Select 5 -> Exit menu");
+			System.out.println("Select 1 -> Display Files\n" + "Select 2 -> To Perform File operations\n"
+					+ "Select 3 -> Exit menu");
 			int input = inp.nextInt();
 			Terminate_or_continue terminateOrContinue = new Terminate_or_continue();
 			switch (input) {
@@ -19,43 +19,8 @@ public class Main {
 				System.out.println(Listoffiles.list);
 				terminateOrContinue.terminateOrContinue();
 				break;
-			case 2:
-				System.out.println("Press s : To Enter Single File\n" + "Press m :To Enter Multiple File");
-				try {
-					String n = inp.next();
-					if (n.equals("s")) {
-						System.out.println("Enter the file name :");
-						new Addfile(inp.next());
-						break;
-					}
-					if (n.equals("m")) {
-						System.out.println("Enter the Number of Files to Enter :");
-						int nofile = inp.nextInt();
-						new Addmultiplefile(nofile);
-						break;
-					}
-				} catch (InputMismatchException e) {
-					e.getMessage();
-					
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
+			case 2:new File_operations().file_operations();;
 			case 3:
-				System.out.println("Enter the File to Remove");
-				try {
-					String file = inp.next();
-					new Deletefile(file);
-					terminateOrContinue.terminateOrContinue();
-					break;
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			case 4:
-				System.out.println("Enter the File to Search");
-				String searchf = inp.next();
-				new Search_file(searchf);
-
-			case 5:
 				return;
 
 			default:
